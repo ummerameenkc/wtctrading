e0fe0ee494de17ca55c98de1fb09d478ec4b3f0a
ummerameenkc <ummerameen.kc@gmail.com>
2025-08-02 21:06:13 +0400

    Update dbConfig.js

--- src/utils/dbConfig.js
+++ src/utils/dbConfig.js
@@ -3,7 +3,11 @@ let config = {
   user: "webzyspa_wtctrading",
   password: "$45p[vkSr0tBL$Mm",
   database: "webzyspa_wtctrading",
-  dialect: 'mysql' 
+  dialect: 'mysql' ,
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 }
 
   module.exports = {config}
